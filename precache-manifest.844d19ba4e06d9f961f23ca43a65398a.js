fe9b7a04d314c5de74e9e818c37ed8a0ae7389f4
Steven Liao <stevenliao19900326@gmail.com>
2019-07-06 14:45:58 +1000

    Updates

---
+++ precache-manifest.844d19ba4e06d9f961f23ca43a65398a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5adede6c5527287ddbb57adec9fd19",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "6102e7fbaf23f9baed1a",
+    "url": "/MyPortfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "36b92a96e90c2fb1cc0f",
+    "url": "/MyPortfolio/static/css/main.d8422498.chunk.css"
+  },
+  {
+    "revision": "6102e7fbaf23f9baed1a",
+    "url": "/MyPortfolio/static/js/2.b6dff942.chunk.js"
+  },
+  {
+    "revision": "36b92a96e90c2fb1cc0f",
+    "url": "/MyPortfolio/static/js/main.9247a8ce.chunk.js"
+  },
+  {
+    "revision": "41ef2c5e8e68d7a2a057",
+    "url": "/MyPortfolio/static/js/runtime~main.ccce04bf.js"
+  }
+]);
